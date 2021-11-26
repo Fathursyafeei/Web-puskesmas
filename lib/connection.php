78dbb8f9c2c3f567a9dfbe32f3a9d99acf07dc7d
rzkytmgr <rzkytmgr@gmail.com>
2021-11-27 06:04:11 +0700

    :surfer: add helper class and connection to database

---
+++ lib/connection.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+
+$DB_USERNAME = ''; // ENTRAR YOUR USERNAME
+$DB_PASSWORD = ''; // ENTRAR YOUR PASSWORD
+$DB_PORT = "3306"; // CHANGE TO YOUR PORT
+$DB_HOST = "localhost"; // CHANGE TO YOUR HOST
+$DB_NAME = "puskesmas_kentara";
+
+$connection = mysqli_connect($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
+
+if(!$connection)
+    die('Database connection error');
---
+++ lib/helper.php
@@ -0,0 +1,22 @@
+<?php
+
+class Helper {
+
+    public static function arrayInputSanitizer($data, $conn) {
+        if(!is_array($data)) die('hanya array yang diterima');
+        if(empty($data)) return $data;
+
+        $temp = [];
+        foreach($data as $key => $value) {
+            if($key == 'password') {
+                $temp[$key] = password_hash($value, PASSWORD_DEFAULT);
+                continue;
+            }
+            
+            $temp[$key] = mysqli_real_escape_string($conn, htmlspecialchars(strip_tags($value, ''), ENT_QUOTES));
+        }
+
+        return $temp;
+    }
+
+}
